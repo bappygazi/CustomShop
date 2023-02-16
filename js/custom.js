8d413e3819f661c18e47bb5606db9c0c4889bc2f
Bappy Gazi <41175073+bappygazi@users.noreply.github.com>
2023-02-16 13:31:09 +0600

    added signUp page also

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// $(function () {
+//     // setTimout is not necessary
+//     setTimeout(function () {
+
+//         $('.recaptcha').each(function () {
+//             grecaptcha.render(this.id, {
+//                 'sitekey': '6LdVkwkUAAAAACeeETRX--v9Js0vWyjQOTIZxxeB',
+//                 "theme": "light"
+//             });
+//         });
+
+//     }, 2000);
+// });
